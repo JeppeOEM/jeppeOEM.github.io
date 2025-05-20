72200ba48fad943caf549c26a551548a948252c4
JeppeOEM <jappa669@gmail.com>
2025-05-20 19:34:32 +0200

    blue bg

--- js/linksPage.js
+++ js/linksPage.js
@@ -1,7 +1,32 @@
 import AsciiLogoBackground from "./AsciiLogoBackground.js";
+import AsciiBackground from "./AsciiBackground.js";
 import { leftPre, rightPre } from "./linkBackground.js";
+import { dotPattern } from "./dotPattern.js"
+import { horizontalBinaryAnimation, startBinaryAnimation } from "./binaryAnimation.js";
+
+
 
 export function linksPage() {
+
+
+
+
+
+    const bodyBackground = new AsciiBackground({
+        asciiArt: dotPattern,
+        container: document.body,
+        style: {
+            color: 'var(--blue)',
+            opacity: 1,
+            zIndex: -1
+        }
+    });
+
+    if (bodyBackground) {
+        const background = document.querySelector(".ascii-background")
+        background.classList.add("fade-in-bg")
+    }
+
 
     const leftSection = document.getElementById('leftSection');
     const rightSection = document.getElementById('rightSection');
