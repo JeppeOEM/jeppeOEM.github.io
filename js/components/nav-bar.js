3cbdbc6d2a8775cc4063077202cf3afcbef5f2f1
JeppeOEM <jappa669@gmail.com>
2025-05-22 19:31:50 +0200

    clean up

---
+++ js/components/nav-bar.js
@@ -0,0 +1,47 @@
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+      <nav class="nav">
+        <div class="navbar-side"></div>
+        <div class="navbar">
+          <div>
+            <a href="/" class="nav__link"><span class="bracket">[</span>HOME<span class="bracket">]</span></a>
+            <a href="code.html" class="nav__link" data-link><span class="bracket">[</span>CODE<span class="bracket">]</span></a>
+            <a href="/links.html" class="nav__link" data-link><span class="bracket">[</span>LINKS<span class="bracket">]</span></a>
+          </div>
+          <div>
+            <span class="font-controls">
+              <span class="bracket">[</span>
+              <label for="font-selector">FONT:</label>
+              <select id="font-selector">
+                <option value="IBMVGA8">IBM VGA 8x16</option>
+                <option value="IBMBIOS2Y">IBM BIOS-2y</option>
+              </select>
+              <span class="bracket">]</span>
+            </span>
+          </div>
+        </div>
+        <div class="navbar-side"></div>
+      </nav>
+    `;
+
+        const fontSelector = this.querySelector('#font-selector');
+
+        // Get font from localStorage
+        const savedFont = localStorage.getItem('selectedFont');
+        if (savedFont && fontSelector) {
+            fontSelector.value = savedFont;
+
+            // Optionally apply it globally
+            document.body.style.fontFamily = savedFont;
+        }
+
+        // Listen for changes and update localStorage
+    }
+}
+
+customElements.define('nav-bar', NavBar);
