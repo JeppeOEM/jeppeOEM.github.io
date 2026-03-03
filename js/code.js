92efe11b41950b40fa0e9183d265173baaaf7b43
JeppeOEM <jappa669@gmail.com>
2026-03-03 18:49:17 +0100

    css

--- js/code.js
+++ js/code.js
@@ -37,54 +37,54 @@ export function codePage() {
   logoBackground.fillSections();
   logoBackground.init();
 
-  //const templateClosestChild = document.querySelector(".outer");
-  //const template = document.getElementById("outer-template");
-  //  const asciiBox = new AsciiBox({
-  //    templateClosestChild: templateClosestChild,
-  //    template: template,
-  //    mobileBreakpoint: {
-  //      horizontalChars: 42,
-  //      verticalLines: 20,
-  //      breakpoint: 480,
-  //    },
-  //    tabletBreakpoint: {
-  //      horizontalChars: 42,
-  //      verticalLines: 20,
-  //      breakpoint: 481,
-  //    },
-  //    desktopBreakpoint: {
-  //      horizontalChars: 80,
-  //      verticalLines: 5,
-  //    },
-  //    delay: 8800,
-  //    styleTextContent: `.fade-in-box {
-  //    opacity: 0;
-  //    transform: translateY(200%) scale(0.5);
-  //    animation: fadeSlideScale 1500ms ease forwards;
-  //    animation-delay: 3500ms; /* 4 second delay */
-  //}
-  //    .fade-in-box.show {
-  //        /* Triggers the animation */
-  //    }
-  //
-  //    @keyframes fadeSlideScale {
-  //        0% {
-  //            opacity: 0;
-  //            transform: translateY(200%) scale(0.5);
-  //        }
-  //        50% {
-  //            opacity: 1;
-  //            transform: translateY(0%) scale(0.5); /* Finished sliding, no scale yet */
-  //        }
-  //        100% {
-  //            opacity: 1;
-  //            transform: translateY(0%) scale(1); /* Scale up in place */
-  //        }
-  //    }
-  //`,
-  //  });
-  //
-  //  asciiBox.init();
+  const templateClosestChild = document.querySelector(".outer");
+  const template = document.getElementById("outer-template");
+  const asciiBox = new AsciiBox({
+    templateClosestChild: templateClosestChild,
+    template: template,
+    mobileBreakpoint: {
+      horizontalChars: 42,
+      verticalLines: 20,
+      breakpoint: 480,
+    },
+    tabletBreakpoint: {
+      horizontalChars: 42,
+      verticalLines: 20,
+      breakpoint: 481,
+    },
+    desktopBreakpoint: {
+      horizontalChars: 80,
+      verticalLines: 5,
+    },
+    delay: 8800,
+    styleTextContent: `.fade-in-box {
+      opacity: 0;
+      transform: translateY(200%) scale(0.5);
+      animation: fadeSlideScale 1500ms ease forwards;
+      animation-delay: 3500ms; /* 4 second delay */
+  }
+      .fade-in-box.show {
+          /* Triggers the animation */
+      }
+
+      @keyframes fadeSlideScale {
+          0% {
+              opacity: 0;
+              transform: translateY(200%) scale(0.5);
+          }
+          50% {
+              opacity: 1;
+              transform: translateY(0%) scale(0.5); /* Finished sliding, no scale yet */
+          }
+          100% {
+              opacity: 1;
+              transform: translateY(0%) scale(1); /* Scale up in place */
+          }
+      }
+  `,
+  });
+
+  asciiBox.init();
   //
   //  const leftDiv = document.querySelector(".leftDiv");
   //  startBinaryAnimation(leftDiv, 3, 600);
