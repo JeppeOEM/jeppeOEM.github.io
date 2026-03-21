6dd32377c458085c08fdafa3b30a396d33eae667
JeppeOEM <jappa669@gmail.com>
2026-03-21 21:04:38 +0100

    clean up

--- js/code.js
+++ js/code.js
@@ -43,44 +43,46 @@ export function codePage() {
     template: template,
     mobileBreakpoint: {
       horizontalChars: 42,
-      verticalLines: 20,
+      verticalLines: 10,
+      verticalHeaderLines: 8,
       breakpoint: 480,
     },
     tabletBreakpoint: {
-      horizontalChars: 42,
+      horizontalChars: 60,
       verticalLines: 20,
-      breakpoint: 481,
+      verticalHeaderLines: 0,
+      breakpoint: 768,
     },
     desktopBreakpoint: {
       horizontalChars: 80,
-      verticalLines: 5,
+      verticalHeaderLines: 0,
+      verticalLines: 10,
     },
-    delay: 8800,
     styleTextContent: `.fade-in-box {
-      opacity: 0;
-      transform: translateY(200%) scale(0.5);
-      animation: fadeSlideScale 1500ms ease forwards;
-      animation-delay: 1500ms;
-  }
-      .fade-in-box.show {
-          /* Triggers the animation */
-      }
+    opacity: 0;
+    transform: translateY(200%) scale(0.5);
+    animation: fadeSlideScale 1500ms ease forwards;
+    animation-delay: 1500ms;
+}
+    .fade-in-box.show {
+        /* Triggers the animation */
+    }
 
-      @keyframes fadeSlideScale {
-          0% {
-              opacity: 0;
-              transform: translateY(200%) scale(0.5);
-          }
-          50% {
-              opacity: 1;
-              transform: translateY(0%) scale(0.5); /* Finished sliding, no scale yet */
-          }
-          100% {
-              opacity: 1;
-              transform: translateY(0%) scale(1); /* Scale up in place */
-          }
-      }
-  `,
+    @keyframes fadeSlideScale {
+        0% {
+            opacity: 0;
+            transform: translateY(200%) scale(0.5);
+        }
+        50% {
+            opacity: 1;
+            transform: translateY(0%) scale(0.5); /* Finished sliding, no scale yet */
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0%) scale(1); /* Scale up in place */
+        }
+    }
+`,
   });
 
   asciiBox.init();
